3a277e552e286bc873b9f706dbf8f611f3ddbdbd
jacdoyle-jpg <jacdoyle@clarkson.edu>
2026-03-13 17:39:37 -0400

    Add files via upload

--- Project3/Proj3.py
+++ Project3/Proj3.py
@@ -1,16 +1,16 @@
 #Part 1
 import pandas as pd
 import numpy as np
-df = pd.read_csv ("train.csv")
+df = pd.read_csv("winequality-white.csv", sep=';')
 print(df.shape)
 print(df.head())
 print(df.info())
-missing = df.isnull().sum ()
-missing = missing [missing > 0]
-print(missing.sort_values(ascending = False))
+missing = df.isnull().sum()
+missing = missing[missing > 0]
+print(missing.sort_values(ascending=False))
 
 #Part 2
-target_col = "SalePrice"
+target_col = "quality"
 # Separate numeric and categorical columns
 numeric_cols = df.select_dtypes(include=[np.number]).columns.tolist()
 categorical_cols = df.select_dtypes(exclude=[np.number]).columns.tolist()
@@ -18,34 +18,32 @@ categorical_cols = df.select_dtypes(exclude=[np.number]).columns.tolist()
 if target_col in numeric_cols:
     numeric_cols.remove(target_col)
 # Fill numeric missing values with median
-for col in numeric_cols :
+for col in numeric_cols:
     df[col] = df[col].fillna(df[col].median())
 # Fill categorical missing values with mode
-for col in categorical_cols :
+for col in categorical_cols:
     if df[col].isnull().any():
         df[col] = df[col].fillna(df[col].mode()[0])
-# One - hot encoding
-df = pd.get_dummies(df,drop_first = True)
+# One-hot encoding
+df = pd.get_dummies(df, drop_first=True)
 # Define X and y
-X = df.drop("SalePrice", axis=1)
-y = df["SalePrice"]
+X = df.drop("quality", axis=1)
+y = df["quality"]
 # Scale features
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 #Part 3
-U, S, Vt = np.linalg.svd(X_scaled, full_matrices = False)
-explained_variance = S**2/np.sum(S **2)
+U, S, Vt = np.linalg.svd(X_scaled, full_matrices=False)
+explained_variance = S**2 / np.sum(S**2)
 cumulative_variance = np.cumsum(explained_variance)
 # Choose k to retain at least 90% variance
 k = np.argmax(cumulative_variance >= 0.90) + 1
 X_reduced = U[:, :k] @ np.diag(S[:k])
 
-#Variance 
-# Explained Variance Plot
+#Explained Variance Plot
 import matplotlib.pyplot as plt
-
 plt.plot(range(1, len(cumulative_variance)+1), cumulative_variance, marker='o')
 plt.axhline(y=0.90, linestyle='--')
 plt.xlabel("Number of Principal Components")
